4b475fc6abd3d93ed744f28a702729875a801580
dbzerjav <dbzerjav@gmail.com>
2016-04-27 17:49:53 +0000

    fixing bug with stripe charges

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -30,10 +30,10 @@ class RegistrationsController < Devise::RegistrationsController
           expire_data_after_sign_in!
           respond_with resource, location: after_inactive_sign_up_path_for(resource)
         end
-        else
-          clean_up_passwords resource
-          set_minimum_password_length
-          respond_with resource
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
       end
     end
   end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :phblisable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
   :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
-}
+  }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
